2e273afda338d89f293bd67103622dc171ffe6ac
Leo P <leo.punsola@gmail.com>
2018-04-03 23:41:36 +0200

    Added the 'fireball' animation with gravity

--- js/level.js
+++ js/level.js
@@ -5,85 +5,120 @@ canvas.height = 600;
 var ctx = canvas.getContext("2d");
 
 var levelMap = [
-            "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-            "xxxxxxxxxxxxxxxxxxx vvvvvvvvvv                              ",
-            "                                                         #  ",
-            "vvvxxxvvv vv vv vvv                    o o o o o            ",  
-            " o xxx                         xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-            "                       xxx     xvvvvv                 vvvv  ",
-            "                               x                            ",
-            "      xxxxxxxxxxxxx     o      x                            ",
-            "      x!! vvv     x !!!!!!!!!!!x     xxxxxxxxxxxxxxxxx     o",
-            "   xxxx!!         x            x     xv v                   ",
-            "   x!!!!!o   xx   x           ox     x                      ",
-            "   x!!!!!x   xx   xxxxxxxxxxxxxx     x                      ",
-            "   x!!!!!!!!!xx   xxxxxxxxxxxxxx     x   xxxxxxxxxxxxxxxxxxx",
-            "   xxxxxxxxxxxx   vvvv                 x                    ",
-            "                                     x                      ",
-            "                      !!!!!!!!!!!!!!!x                      ",
-            "                      xxxxxxxxxxxxxxxxxxxxxxxxxxxx   xxxxxxx",
-            " x xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   v   x   vvv vvv",
-            "   v   v    v   v     xxxxxxxxxxxxxxxxxxxx       x      o   ",
-            "                      xxxxvvvvvxxvvvvvxxxx       x      x   ",
-            "                      xxxxo           xxxx       x   xxxxxxx",
-            "!!!!!!!!! !!!!!!!!! !!!!!x            !!!x   x   x   vvv vvv",
-            "!!!!!!!!! !!!!!!!!!      x               x   x   x          ",
-            "!!!!!!!!!o!!!!!!!!!     ox             o x   x   v      o   ",
-            "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxx   x          x   ",
-            "         v        v               v  v  v    x    !!!!!!!!!!",
-            "                                             x              ",
-            "                                             x             o",
-            "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-            "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
-        ];
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "xxxxxxxxxxxxxxxxxxx vvvvvvvvvv                              ",
+  "                                                         #  ",
+  "vvvxxxvvv vv vv vvv                    o o o o o            ",
+  " o xxx                         xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "                       xxx     xvvvvv                 vvvv  ",
+  "                               x                            ",
+  "      xxxxxxxxxxxxx     o      x                            ",
+  "      x!! vvv     x !!!!!!!!!!!x     xxxxxxxxxxxxxxxxx     o",
+  "   xxxx!!         x            x     xv v                   ",
+  "   x!!!!!o   xx   x           ox     x                      ",
+  "   x!!!!!x   xx   xxxxxxxxxxxxxx     x                      ",
+  "   x!!!!!!!!!xx   xxxxxxxxxxxxxx     x   xxxxxxxxxxxxxxxxxxx",
+  "   xxxxxxxxxxxx   vvvv                 x                    ",
+  "                                     x                      ",
+  "                      !!!!!!!!!!!!!!!x                      ",
+  "                      xxxxxxxxxxxxxxxxxxxxxxxxxxxx   xxxxxxx",
+  " x xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   v   x   vvv vvv",
+  "   v   v    v   v     xxxxxxxxxxxxxxxxxxxx       x      o   ",
+  "                      xxxxvvvvvxxvvvvvxxxx       x      x   ",
+  "                      xxxxo           xxxx       x   xxxxxxx",
+  "!!!!!!!!! !!!!!!!!! !!!!!x            !!!x   x   x   vvv vvv",
+  "!!!!!!!!! !!!!!!!!!      x               x   x   x          ",
+  "!!!!!!!!!o!!!!!!!!!     ox             o x   x   v      o   ",
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxx   x          x   ",
+  "         v        v               v  v  v    x    !!!!!!!!!!",
+  "                                             x              ",
+  "                                             x             o",
+  "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
+  "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+];
 
-
+// Player initialization
 var player = {
-    x: (3*20)+3,
-    y: (26*20)+5,
-    width: 15,
-    height: 30,
-    color: "#FF6714",
-    drawMe : function() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
+  x: 3 * 20 + 3,
+  y: 26 * 20 + 5,
+  width: 15,
+  height: 30,
+  color: "#FF6714",
+  drawMe: function() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
 };
 
-var body = document.querySelector("body");
-    body.onkeydown = function() {
-        switch (event.keyCode) {
-            case 32:
-            case 87:
-            case 38:
-            player.y -= 10;
-            break;
+// Fireball initialization
 
-            case 65:
-            case 37:
-            player.x -= 10;
-            break;
+var fireBallPosX = 0;
+var fireBallPosY = 0;
+var fireBallDirX = 0;
+var fireBallDirY = 0;
 
-            case 83:
-            case 40:
-            player.y += 10;
-            break;
+var fireBalls = [];
 
-            case 68:
-            case 39:
-            player.x += 10;
-            break;
-        }
+function FireBall(myX, myY, myW, myH) {
+  this.x = myX;
+  this.y = myY;
+  this.width = myW;
+  this.height = myH;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.gravity = 0.05;
+  this.gravitySpeed = 0;
+  this.update = function() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.newPos = function() {
+      this.gravitySpeed += this.gravity;
+      this.x += this.speedX;
+      this.y += this.speedY + this.gravitySpeed;
     };
+  };
+}
 
+FireBall.prototype.drawBall = function() {
+  ctx.fillStyle = "#FFF";
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+};
 
+// Walls
 
+// Collision detection
+
+// User interactions
+
+var body = document.querySelector("body");
+body.onkeydown = function() {
+  switch (event.keyCode) {
+    case 32:
+    case 87:
+    case 38:
+      player.y -= 10;
+      break;
+
+    case 65:
+    case 37:
+      player.x -= 10;
+      break;
+
+    case 83:
+    case 40:
+      player.y += 10;
+      break;
+
+    case 68:
+    case 39:
+      player.x += 10;
+      break;
+  }
+};
 
 // legend:
 //          x = platform
 //          ! = lava
 //          @ = player starting point
 //          v = falling block
-//          o = relics 
+//          o = relics
 //          # = prisoner that must be saved by collecting all relics
-
--- js/script.js
+++ js/script.js
@@ -1,65 +1,64 @@
+var mapGenerated = false;
 
-function drawMap(levelArray){
-    for (var y=0; y<levelArray.length; y++){
-        var line = levelArray[y];
-        // console.log(line.length);
-        for (var x = 0; x<line.length; x++){
-            var ch = line[x];
-            // console.log(ch);
-            if (ch === "x"){
-                // identify as wall + draw tile 20 by 20
-                ctx.fillStyle = "#C9D6D4";
-                ctx.fillRect(x*20,y*20,20,20);
-            }
-            else if (ch === "!"){
-                // identify as acid + draw tile 20 by 20
-                ctx.fillStyle = "#7DFF00";
-                ctx.fillRect(x*20,y*20,20,20);              
-            }
-            else if (ch === "#"){
-                // identify as captive + draw tile
-                ctx.fillStyle = "#2436E8";
-                ctx.fillRect(x*20,y*20,15,30);
-            }
-            else if (ch === "o"){
-                // identify as relics
-                ctx.fillStyle = "#E8E300";
-                ctx.fillRect((x*20)+5,y*20,10,10);
-                
-
-            }
-            else if (ch === "v"){
-                // identify as falling blocks
-            }
-            else {
-                // identify as empty space
-            }
+function drawMap(levelArray) {
+  for (var y = 0; y < levelArray.length; y++) {
+    var line = levelArray[y];
+    for (var x = 0; x < line.length; x++) {
+      var ch = line[x];
+      if (ch === "x") {
+        // identify as wall + draw tile 20 by 20
+        ctx.fillStyle = "#C9D6D4";
+        ctx.fillRect(x * 20, y * 20, 20, 20);
+      } else if (ch === "!") {
+        // identify as acid + draw tile 20 by 20
+        ctx.fillStyle = "#7DFF00";
+        ctx.fillRect(x * 20, y * 20, 20, 20);
+      } else if (ch === "#") {
+        // identify as captive + draw tile
+        ctx.fillStyle = "#2436E8";
+        ctx.fillRect(x * 20, y * 20, 15, 30);
+      } else if (ch === "o") {
+        // identify as relics
+        ctx.fillStyle = "#E8E300";
+        ctx.fillRect(x * 20 + 5, y * 20, 10, 10);
+      } else if (ch === "v") {
+        // identify as falling blocks
+        fireBallPosX = x * 20;
+        fireBallPosY = y * 20;
+        if (mapGenerated === false) {
+          fireBalls.push(new FireBall(fireBallPosX, fireBallPosY, 10, 10));
         }
+      } else {
+      }
     }
-
-};
-
-
-function updateStuff () {
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-    drawMap(levelMap)
-    player.drawMe();
-  
-    requestAnimationFrame(function () {
-      updateStuff();
-    });
   }
-  
-  updateStuff();
+  mapGenerated = true;
+}
 
+function updateStuff() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawMap(levelMap);
 
+  player.drawMe();
 
-// Next steps: 
+  fireBalls.forEach(function(fireBall) {
+    fireBall.drawBall();
+    fireBall.update();
+    fireBall.newPos();
+  });
+
+  requestAnimationFrame(function() {
+    updateStuff();
+  });
+}
+
+updateStuff();
+
+// Next steps:
 // 1. define a constructor collision function to apply to each tile of the map
 // 2. create and initialize player and player movements
 // 3. animate lava
 // 4. animate coins with wobble
 // 5. animate captive delivrance
 
-
-drawMap(levelMap)
+// drawMap(levelMap);
