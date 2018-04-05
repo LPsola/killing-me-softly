ed1c61afbd900150fef55bceb6c3f0063d3f255e
Leo P <leo.punsola@gmail.com>
2018-04-05 17:30:48 +0200

    added collisions and winning log, still have to make start and end menus

--- js/level.js
+++ js/level.js
@@ -23,16 +23,16 @@ var levelMap = [
   "                      !!!!!!!!!!!!!!!x                      ",
   "                      xxxxxxxxxxxxxxxxxxxxxxxxxxxx   xxxxxxx",
   " x xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx   v   x   vvv vvv",
-  "   v   v    v   v     xxxxxxxxxxxxxxxxxxxx       x      o   ",
-  "                      xxxxvvvvvxxvvvvvxxxx       x      x   ",
-  "                      xxxxo           xxxx       x   xxxxxxx",
-  "!!!!!!!!! !!!!!!!!! !!!!!x            !!!x   x   x   vvv vvv",
+  "   v   v    v   v     xxxx!!!!!xx!!!!!xxxx       x      o   ",
+  "                      xxxx vvvvxxvvvvvxxxx       x      x   ",
+  "                      xxxxo           xxxx       x   !!!x!!!",
+  "!!!!!!!!! !!!!!!!!! !!!!!x            !!!x   x   x          ",
   "!!!!!!!!! !!!!!!!!!      x               x   x   x          ",
   "!!!!!!!!!o!!!!!!!!!     ox             o x   x   v      o   ",
   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx  xxxxxxxxx   x          x   ",
   "         v        v               v  v  v    x    !!!!!!!!!!",
   "                                             x              ",
-  "                                             x             o",
+  "         o                                   x             o",
   "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
   "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 ];
@@ -43,35 +43,75 @@ var player = {
   y: 26 * 20 + 5,
   width: 15,
   height: 30,
+  speedX: 1,
+  speedY: 1,
+  acceleration: 0.2,
+  maxSpeed: 5,
   color: "#FF6714",
+  wallCollision: false,
+  coinCollision: false,
+  acidCollision: false,
+  fireBallCollision: false,
   drawMe: function() {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 };
 
+function playerMoveX() {
+  while (player.speedX <= player.maxSpeed) {
+    player.speedX += player.acceleration;
+  }
+}
+
+function playerMoveY() {
+  while (player.speedY <= player.maxSpeed) {
+    player.speedY += player.acceleration;
+  }
+}
+// to be deleted var oldPosX;
+// to be deleted var oldPosY;
+var playerPosX = [player.x];
+var playerPosY = [player.y];
+var topOrBottom = [];
+var leftOrRight = [];
+
+// to be deleted var playerCol = [player.wallCollision];
+
+// to be deleted function testPos() {
+//  to be deleted  console.log(playerPosY);
+// to be deleted }
+
+// to be deleted function testCol() {
+// to be deleted  console.log(playerCol);
+//to be deleted  }
+
+// to be deleted function testTop() {
+// to be deleted   console.log(topOrBottom[topOrBottom.length - 1]);
+//to be deleted  }
+
 // Fireball initialization
 
 var fireBallPosX = 0;
 var fireBallPosY = 0;
-var fireBallDirX = 0;
-var fireBallDirY = 0;
 
 var fireBalls = [];
 
 function FireBall(myX, myY, myW, myH) {
   this.x = myX;
   this.y = myY;
+  this.ox = myX;
+  this.oy = myY;
   this.width = myW;
   this.height = myH;
+  this.collision = false;
   this.speedX = 0;
   this.speedY = 0;
   this.gravity = 0.05;
   this.gravitySpeed = 0;
   this.update = function() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
     this.newPos = function() {
-      this.gravitySpeed += this.gravity;
+      this.gravitySpeed = 0.7;
       this.x += this.speedX;
       this.y += this.speedY + this.gravitySpeed;
     };
@@ -85,7 +125,141 @@ FireBall.prototype.drawBall = function() {
 
 // Walls
 
+var wallPosX = 0;
+var wallPosY = 0;
+
+var walls = [];
+
+function Wall(myX, myY, myW, myH) {
+  this.x = myX;
+  this.y = myY;
+  this.width = myW;
+  this.height = myH;
+}
+
+function wallCollision() {
+  walls.forEach(function(oneWall) {
+    if (collision(player, oneWall)) {
+      player.wallCollision = true;
+      playerPosY.push(player.y);
+      playerPosX.push(player.x);
+      // to be deleted  playerCol.push(player.wallCollision);
+    }
+    fireBalls.forEach(function(oneFireBall) {
+      if (collision(oneFireBall, oneWall) || oneFireBall.y > canvas.height) {
+        oneFireBall.collision = true;
+        oneFireBall.y = oneFireBall.oy;
+      }
+      if (collision(player, oneFireBall)) {
+        player.fireBallCollision = true;
+        player.x = 3 * 20 + 3;
+        player.y = 26 * 20 + 5;
+      }
+    });
+  });
+}
+
+// Acid
+
+var acidPosX = 0;
+var acidPosY = 0;
+
+var acidArray = [];
+
+function Acid(myX, myY, myW, myH) {
+  this.x = myX;
+  this.y = myY;
+  this.width = myW;
+  this.height = myH;
+}
+
+function acidCollision() {
+  acidArray.forEach(function(oneAcid) {
+    if (collision(player, oneAcid)) {
+      player.acidCollision = true;
+      player.x = 3 * 20 + 3;
+      player.y = 26 * 20 + 5;
+      // to be deleted  playerCol.push(player.wallCollision);
+    }
+  });
+}
+
+// Coins
+
+var coinPosX = 0;
+var coinPosY = 0;
+var totalCoins = 18;
+var coinCounter = 0;
+
+var coins = [];
+
+function Coin(myX, myY, myW, myH) {
+  this.x = myX;
+  this.y = myY;
+  this.width = myW;
+  this.height = myH;
+}
+
+Coin.prototype.drawCoin = function() {
+  ctx.fillStyle = "#E8E300";
+  ctx.fillRect(this.x, this.y, this.width, this.height);
+};
+
+function win() {
+  if (coinCounter === totalCoins) {
+    console.log("win");
+  }
+}
+
+function coinCollision() {
+  // var collidedCoin = [];
+  coins.forEach(function(oneCoin) {
+    if (collision(player, oneCoin)) {
+      coins.splice(coins.indexOf(oneCoin), 1);
+      coinCounter += 1;
+      youWinText();
+
+      // to be deleted  playerCol.push(player.wallCollision);
+    }
+    // collidedCoin.pop();
+  });
+  // collidedCoin.pop();
+}
+
+// text pop up box
+
+function youWinText() {
+  ctx.fillStyle = "#7DFF00";
+  ctx.fillRect(x * 20, y * 20, 100, 100);
+  ctx.font = "80px Verdana";
+  ctx.fillStyle = "white";
+
+  ctx.fillText("You WIN!", 10, 90);
+}
+
 // Collision detection
+
+function getTop(obj) {
+  return obj.y;
+}
+function getBottom(obj) {
+  return obj.y + obj.height;
+}
+function getLeft(obj) {
+  return obj.x;
+}
+function getRight(obj) {
+  return obj.x + obj.width;
+}
+
+function collision(objA, objB) {
+  return (
+    getBottom(objA) >= getTop(objB) &&
+    getTop(objA) <= getBottom(objB) &&
+    getRight(objA) >= getLeft(objB) &&
+    getLeft(objA) <= getRight(objB)
+  );
+}
 
 // User interactions
 
@@ -95,22 +269,77 @@ body.onkeydown = function() {
     case 32:
     case 87:
     case 38:
-      player.y -= 10;
+      if (
+        player.wallCollision === true &&
+        topOrBottom[topOrBottom.length - 1] === "top" &&
+        player.y === playerPosY[playerPosY.length - 1]
+      ) {
+        player.y = playerPosY[playerPosY.length - 1] + player.speedY;
+        player.wallCollision = false;
+        // to be deleted  playerCol.push(player.wallCollision);
+        topOrBottom.push("top");
+        break;
+      }
+      player.y -= player.speedY;
+      playerMoveX();
+      playerMoveY();
+      topOrBottom.push("top");
+      // to be deleted  playerPosY.push(player.y);
       break;
-
     case 65:
     case 37:
-      player.x -= 10;
+      if (
+        player.wallCollision === true &&
+        leftOrRight[leftOrRight.length - 1] === "left" &&
+        player.x === playerPosX[playerPosX.length - 1]
+      ) {
+        player.x = playerPosX[playerPosX.length - 1] + player.speedX;
+        player.wallCollision = false;
+        // to be deleted  playerCol.push(player.wallCollision);
+        leftOrRight.push("left");
+        break;
+      }
+      player.x -= player.speedX;
+      playerMoveX();
+      playerMoveY();
+      leftOrRight.push("left");
       break;
-
     case 83:
     case 40:
-      player.y += 10;
+      if (
+        player.wallCollision === true &&
+        topOrBottom[topOrBottom.length - 1] === "bottom" &&
+        player.y === playerPosY[playerPosY.length - 1]
+      ) {
+        player.y = playerPosY[playerPosY.length - 1] - player.speedY;
+        player.wallCollision = false;
+        // to be deleted  playerCol.push(player.wallCollision);
+        topOrBottom.push("bottom");
+        break;
+      }
+      player.y += player.speedY;
+      playerMoveX();
+      playerMoveY();
+      topOrBottom.push("bottom");
+      // to be deleted  playerPosY.push(player.y);
       break;
-
     case 68:
     case 39:
-      player.x += 10;
+      if (
+        player.wallCollision === true &&
+        leftOrRight[leftOrRight.length - 1] === "right" &&
+        player.x === playerPosX[playerPosX.length - 1]
+      ) {
+        player.x = playerPosX[playerPosX.length - 1] - player.speedX;
+        player.wallCollision = false;
+        // to be deleted  playerCol.push(player.wallCollision);
+        leftOrRight.push("right");
+        break;
+      }
+      player.x += player.speedX;
+      playerMoveX();
+      playerMoveY();
+      leftOrRight.push("right");
       break;
   }
 };
--- js/script.js
+++ js/script.js
@@ -9,24 +9,39 @@ function drawMap(levelArray) {
         // identify as wall + draw tile 20 by 20
         ctx.fillStyle = "#C9D6D4";
         ctx.fillRect(x * 20, y * 20, 20, 20);
+        wallPosX = x * 20;
+        wallPosY = y * 20;
+        if (mapGenerated === false) {
+          walls.push(new Wall(wallPosX, wallPosY, 20, 20));
+        }
       } else if (ch === "!") {
         // identify as acid + draw tile 20 by 20
         ctx.fillStyle = "#7DFF00";
         ctx.fillRect(x * 20, y * 20, 20, 20);
+        acidPosX = x * 20;
+        acidPosY = y * 20;
+        if (mapGenerated === false) {
+          acidArray.push(new Acid(acidPosX, acidPosY, 20, 20));
+        }
       } else if (ch === "#") {
         // identify as captive + draw tile
         ctx.fillStyle = "#2436E8";
         ctx.fillRect(x * 20, y * 20, 15, 30);
       } else if (ch === "o") {
         // identify as relics
-        ctx.fillStyle = "#E8E300";
-        ctx.fillRect(x * 20 + 5, y * 20, 10, 10);
+        // ctx.fillStyle = "#E8E300";
+        // ctx.fillRect(x * 20 + 5, y * 20, 10, 10);
+        coinPosX = x * 20 + 5;
+        coinPosY = y * 20;
+        if (mapGenerated === false) {
+          coins.push(new Coin(coinPosX, coinPosY, 10, 10));
+        }
       } else if (ch === "v") {
         // identify as falling blocks
         fireBallPosX = x * 20;
         fireBallPosY = y * 20;
         if (mapGenerated === false) {
-          fireBalls.push(new FireBall(fireBallPosX, fireBallPosY, 10, 10));
+          fireBalls.push(new FireBall(fireBallPosX + 5, fireBallPosY, 10, 10));
         }
       } else {
       }
@@ -37,19 +52,38 @@ function drawMap(levelArray) {
 
 function updateStuff() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // map initialisation
   drawMap(levelMap);
 
+  // player initialisation
   player.drawMe();
+  // to be deleted  testPos();
+  // to be deleted  testCol();
+  // to be deleted  testTop();
 
+  // collisions
+  wallCollision();
+  acidCollision();
+  coinCollision();
+
+  // falling fireBalls
   fireBalls.forEach(function(fireBall) {
     fireBall.drawBall();
     fireBall.update();
     fireBall.newPos();
   });
 
+  // coins
+  coins.forEach(function(oneCoin) {
+    oneCoin.drawCoin();
+  });
+
   requestAnimationFrame(function() {
     updateStuff();
   });
+
+  win();
 }
 
 updateStuff();
